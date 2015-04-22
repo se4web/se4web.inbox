4c169eb2c3c62361da47478d83ef4e7a78606344
se4web <se4web@gmail.com>
2015-04-21 23:44:31 -0700

    Page extends Loadable works! :)

---
+++ src/test/java/se4web/TestHomePage.java
@@ -0,0 +1,28 @@
+package se4web;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import se4web.pages.HomePage;
+import se4web.pages.LoginPage;
+
+public class TestHomePage extends TestNgTestBase {
+
+  protected LoginPage loginpage;
+  protected HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+//    loginpage = PageFactory.initElements(driver, LoginPage.class);
+    loginpage = new LoginPage(driver);
+    homepage = new HomePage(driver, loginpage);
+    homepage.get();
+  }
+
+  @Test (priority=1)
+  public void testLoginPageHasTitle() {
+//    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.getTitle()));
+  }
+
+}
